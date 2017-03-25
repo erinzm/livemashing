75f40583e77b222249d8b2a283b92e630108261e
Liam <liam@cpan.org>
2017-03-25 18:10:45 -0500

    Initial commit

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+	name='livemashing',
+	author='Liam Marshall',
+	author_email='limarshall@wisc.edu',
+	version='0.1',
+	license='GPLv3',
+
+	packages=['livemashing'],
+	install_requires=[
+		'coloredlogs',
+		'click',
+		'PyYAML',
+
+		'mido'
+	],
+
+	entry_points='''
+	[console_scripts]
+	livemash=livemashing.cli:livemash
+	'''
+)
